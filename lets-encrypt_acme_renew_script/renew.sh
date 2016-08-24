911496348d5049c88e4e9ff9e13c88ee0dc4d2b2
Frezbo <git@frezbo.com>
2016-08-24 22:19:29 +0530

    Fixed directory locations

--- lets-encrypt_acme_renew_script/renew.sh
+++ lets-encrypt_acme_renew_script/renew.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #Author: TechnoMan, mail: git@frezbo.com
-
+document_root=/var/www/html # change to your document root
+scripts_directory=/usr/local/scripts # scripts directory
+acme_directory=acme-tiny/lets-encrypt_acme_renew_script
+nginx_config_location=/etc/nginx #change if this is not your config directory
 #certificate file location
-certs_location=/etc/nginx/ssl/certs
+certs_location=${nginx_config_location}/ssl/certs
 #domain keys location
-keys_location=/etc/nginx/ssl/private
+keys_location=${nginx_config_location}/ssl/private
 #file containing list of domains
-domain_file=/usr/local/scripts/acme/domains.txt
+domain_file=${scripts_directory}/${acme_directory}/domains.txt
 #file containing CSR genreation parameters
-csr_params=/usr/local/scripts/acme/domains.cnf
+csr_params=${scripts_directory}/${acme_directory}/domains.cnf
 #intermediate certificate
-int_cert=/usr/local/scripts/acme/intermediate.pem
+int_cert=${scripts_directory}/${acme_directory}/intermediate.pem
 #current unix time
 curr_time=$(date +%s)
-##set umask
-#umask 335
 #check whether certifcate expires within 30 days
 for i in $(cat $domain_file)
 do
@@ -25,15 +26,17 @@ do
 if [ $days_to_expire -le 30 ]
 then
 echo "$days_to_expire days untill expiry"
-openssl genrsa -out /usr/local/scripts/acme/$i.key 4096
+openssl genrsa -out ${scripts_directory}/${acme_directory}/$i.key 4096
 sed -i "8!s/example.com/$i/g" $csr_params
-openssl req -new -config $csr_params -key /usr/local/scripts/acme/$i.key -out /usr/local/scripts/acme/$i.csr
+openssl req -new -config $csr_params -key ${scripts_directory}/${acme_directory}/$i.key -out ${scripts_directory}/${acme_directory}/$i.csr
 sed -i "8!s/$i/example.com/g" $csr_params
 mv ${certs_location}/$i.crt ${certs_location}/$i.crt.old
-mv /usr/local/scripts/acme/$i.key ${keys_location}
-python acme-tiny/acme_tiny.py --account-key /usr/local/scripts/acme/technoman.key --csr /usr/local/scripts/acme/$i.csr --acme-dir /mnt/data/www/.well-known/acme-challenge > /usr/local/scripts/acme/$i.tmp.crt
-cat /usr/local/scripts/acme/$i.tmp.crt $int_cert > ${certs_location}/$i.crt
-rm /usr/local/scripts/acme/$i.tmp.crt
+mv ${keys_location}/$i.key ${keys_location}/$i.key.old
+mv ${scripts_directory}/${acme_directory}/$i.key ${keys_location}
+python ${scripts_directory}/acme-tiny/acme_tiny.py --account-key ${scripts_directory}/${acme_directory}/priv.key --csr ${scripts_directory}/${acme_directory}/$i.csr --acme-dir ${document_root}/.well-known/acme-challenge > ${scripts_directory}/${acme_directory}/$i.tmp.crt
+cat ${scripts_directory}/${acme_directory}/$i.tmp.crt $int_cert > ${certs_location}/$i.crt
+rm ${scripts_directory}/${acme_directory}/$i.tmp.crt
+rm ${scripts_directory}/${acme_directory}/$i.csr
 fi
 done
 sudo nginx -s reload
