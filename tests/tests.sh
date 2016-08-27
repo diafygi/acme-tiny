00102e0e1f66eca90ea6816a883387486c2403f0
Frezbo <git@frezbo.com>
2016-08-27 19:35:41 +0530

    Added sleep time

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,11 +6,13 @@ sudo mkdir -p /var/www/html/.well-known/acme-challenge
 sudo nginx -s reload
 ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record" -H "X-Auth-Email: $mail" -H "X-Auth-Key: $token" -H "Content-Type: application/json" -d "{\"id\":\"$zone_id\",\"type\":\"A\",\"name\":\"test.frezbo.com\",\"content\":\"$ip\"}" > /dev/null 2>&1
+sleep 10
+dig +short @8.8.8.8 A test.frezbo.com
 openssl genrsa -out priv.key
 openssl req -new -newkey rsa:2048 -nodes -subj "/CN=test.frezbo.com" -out sign.csr
 rm -f privkey.pem
 sudo python acme_tiny.py --ca https://acme-staging.api.letsencrypt.org --account-key priv.key --csr sign.csr --acme-dir /var/www/html/.well-known/acme-challenge > test.crt
 rm -f test.crt
-curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record" -H "X-Auth-Email: $mail" -H "X-Auth-Key: $token" -H "Content-Type: application/json" -d "{\"id\":\"$zone_id\",\"type\":\"A\",\"name\":\"test.frezbo.com\",\"content\":\"0.0.0.0\"}" > /dev/null 2>&1
+#curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record" -H "X-Auth-Email: $mail" -H "X-Auth-Key: $token" -H "Content-Type: application/json" -d "{\"id\":\"$zone_id\",\"type\":\"A\",\"name\":\"test.frezbo.com\",\"content\":\"0.0.0.0\"}" > /dev/null 2>&1
 
 
