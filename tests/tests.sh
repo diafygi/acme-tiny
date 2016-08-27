3d99cfaa97338aa80528ef824348d3f67000891c
Frezbo <git@frezbo.com>
2016-08-27 21:18:28 +0530

    debugging

--- tests/tests.sh
+++ tests/tests.sh
@@ -7,6 +7,8 @@ sudo nginx -s reload
 systemctl restart nginx
 sudo ufw allow 80/tcp
 ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+echo "ip: $ip"
+echo "changing dns record"
 curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record" -H "X-Auth-Email: $mail" -H "X-Auth-Key: $token" -H "Content-Type: application/json" -d "{\"id\":\"$zone_id\",\"type\":\"A\",\"name\":\"test.frezbo.com\",\"content\":\"$ip\"}" > /dev/null 2>&1
 dig +short @8.8.8.8 A test.frezbo.com
 curl localhost
