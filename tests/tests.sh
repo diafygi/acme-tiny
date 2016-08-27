c271544f4d579b43d3672f816517f2cd3983e787
Frezbo <git@frezbo.com>
2016-08-27 19:17:25 +0530

    Added missing sudo

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install nginx
-cat > /etc/nginx/sites-enabled/test.frezbo.com << EOF
+sudo cat > /etc/nginx/sites-enabled/test.frezbo.com << EOF
 server {
                 listen 80;
                 server_name test.frezbo.com;
@@ -17,16 +17,16 @@ server {
                 }
 }
 EOF
-mkdir -p /var/www/html/.well-known
-mkdir -p /var/www/html/.well-known/acme-challenge
-nginx -s reload
+sudo mkdir -p /var/www/html/.well-known
+sudo mkdir -p /var/www/html/.well-known/acme-challenge
+sudo nginx -s reload
 ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record" -H "X-Auth-Email: $mail" -H "X-Auth-Key: $token" -H "Content-Type: application/json" -d "{\"id\":\"$zone_id\",\"type\":\"A\",\"name\":\"test.frezbo.com\",\"content\":\"$ip\"}"
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record" -H "X-Auth-Email: $mail" -H "X-Auth-Key: $token" -H "Content-Type: application/json" -d "{\"id\":\"$zone_id\",\"type\":\"A\",\"name\":\"test.frezbo.com\",\"content\":\"$ip\"}" > /dev/null 2>&1
 opemssl genrsa -out priv.key
 openssl req -new -newkey rsa:2048 -nodes -subj "/CN=test.frezbo.com" -out sign.csr
 rm -f privkey.pem
-python acme_tiny.py --ca https://acme-staging.api.letsencrypt.org --account-key priv.key --csr sign.csr --acme-dir /var/www/html/.well-known/acme-challenge > test.crt
+sudo python acme_tiny.py --ca https://acme-staging.api.letsencrypt.org --account-key priv.key --csr sign.csr --acme-dir /var/www/html/.well-known/acme-challenge > test.crt
 rm -f test.crt
-curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record" -H "X-Auth-Email: $mail" -H "X-Auth-Key: $token" -H "Content-Type: application/json" -d "{\"id\":\"$zone_id\",\"type\":\"A\",\"name\":\"test.frezbo.com\",\"content\":\"0.0.0.0\"}"
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record" -H "X-Auth-Email: $mail" -H "X-Auth-Key: $token" -H "Content-Type: application/json" -d "{\"id\":\"$zone_id\",\"type\":\"A\",\"name\":\"test.frezbo.com\",\"content\":\"0.0.0.0\"}" > /dev/null 2>&1
 
 
