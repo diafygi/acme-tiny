b71df0d1d4dc267ab75cd4195b5fe3905e1a52c5
Frezbo <git@frezbo.com>
2016-08-28 23:35:35 +0530

    fixed typo

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,7 +3,8 @@ sudo apt-get -y install unzip
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip
 unzip ngrok.zip ngrok
 chmod u+x ngrok
-./ngrok http 8888 --log stdout --log-format logfmt --log-level debug > url.log &TRAVIS_DOMAIN=$(grep -Eo "Hostname:[a-z0-9]+.ngrok.io" url.log | head -1 | cut -d':' -f2)
-TRAVIS_SESSION=$(openssl rand -base64 32)
+./ngrok http 8888 --log stdout --log-format logfmt --log-level debug > url.log a& 
+#TRAVIS_DOMAIN=$(grep -Eo "Hostname:[a-z0-9]+.ngrok.io" url.log | head -1 | cut -d':' -f2)
+#TRAVIS_SESSION=$(openssl rand -base64 32)
 python ./tests/server.py &
 
