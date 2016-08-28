c26cadcc781d9a51f6ef22b7ddde65dd132e4731
Frezbo <git@frezbo.com>
2016-08-28 23:18:59 +0530

    fixed python server not serving challenges correctly

--- tests/server.py
+++ tests/server.py
@@ -9,8 +9,7 @@ def app(req, resp):
         if hmac.compare_digest(req['QUERY_STRING'], TRAVIS_SESSION):
             body_len = min(int(req['CONTENT_LENGTH']), 90)
             body = req['wsgi.input'].read(body_len).decode("utf8")
-            body = re.sub(r"[^A-Za-z0-9_\-\.]", "_", body)
-            KEY_AUTHORIZATION['uri'] = "/{0}".format(body.split(".", 1)[0])
+            KEY_AUTHORIZATION['uri'] = "/.well-known/acme-challenge/{0}".format(body.split(".", 1)[0])
             KEY_AUTHORIZATION['data'] = body
             resp('201 Created', [])
             return ["".encode("utf8")]
