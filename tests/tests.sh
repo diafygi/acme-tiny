0054e698fb59fce0e459116a75130739df271736
Frezbo <git@frezbo.com>
2016-08-29 16:14:37 +0530

    corrected ENV's

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo apt-get -y install unzip
+sudo apt-get -y install unzip fuse
+sudo chmod a+r /etc/fuse.conf
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip
 unzip ngrok.zip ngrok
 chmod u+x ngrok
-./ngrok http 8888 --log stdout --log-format logfmt --log-level debug > url.log a& 
-#TRAVIS_DOMAIN=$(grep -Eo "Hostname:[a-z0-9]+.ngrok.io" url.log | head -1 | cut -d':' -f2)
-#TRAVIS_SESSION=$(openssl rand -base64 32)
+./ngrok http 8888 --log stdout --log-format logfmt --log-level debug > url.log &
 python ./tests/server.py &
 
