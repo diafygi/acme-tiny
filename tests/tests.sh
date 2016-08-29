73f521a28021955fbde7aba1b889359cbd4f245c
Frezbo <git@frezbo.com>
2016-08-29 16:30:53 +0530

    fixed running python server after setting ENV

--- tests/tests.sh
+++ tests/tests.sh
@@ -5,5 +5,3 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.
 unzip ngrok.zip ngrok
 chmod u+x ngrok
 ./ngrok http 8888 --log stdout --log-format logfmt --log-level debug > url.log &
-python ./tests/server.py &
-
