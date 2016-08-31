9ca772efbc001ae91af25f1518b28a11812a1f66
Frezbo <git@frezbo.com>
2016-08-31 22:51:34 +0530

    Added check for test OS release which was causing failed builds

--- tests/tests.sh
+++ tests/tests.sh
@@ -3,7 +3,10 @@ sudo apt-get -y install unzip nginx
 sudo mkdir -p /var/www/html/.well-known/acme-challenge
 sudo chown -R $(whoami):www-data /var/www/html/.well-known/
 sudo chmod -R 770 /var/www/html/.well-known
-cat /etc/issue
+if [ $(head -n 1 /etc/issue | awk '{print $2}'  | cut -d "." -f1) == 14 ]
+then
+	sed -i '/root \/usr.*/s//root \/var\/www\/html;/g' /etc/nginx/sites-available/default	
+fi
 sudo service nginx restart
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip
 unzip ngrok.zip ngrok
