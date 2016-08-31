263fbbdc06e48bdc5ddb1d569d007d15fed2124d
Frezbo <git@frezbo.com>
2016-08-31 21:14:51 +0530

    testing

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get -y install unzip nginx
-sudo mkdir -p /var/www/html/.well-known/acme-challenge
-sudo chmod -R o+rwx /var/www/html/.well-known
+#sudo mkdir -p /var/www/html/.well-known/acme-challenge
+#sudo chmod -R o+rwx /var/www/html/.well-known
 sudo service nginx restart
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip
 unzip ngrok.zip ngrok
