b88305026cd75ae2fc3d2f048cada3ca1fbed263
Frezbo <git@frezbo.com>
2016-08-31 18:39:55 +0530

    Removed FUSE requirement and added nginx to serve the files

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo apt-get -y install unzip fuse
-sudo chmod a+r /etc/fuse.conf
+sudo apt-get -y install unzip nginx
+sudo mkdir -p /var/www/html/.well-known/acme-challenge
+sudo service nginx restart
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip
 unzip ngrok.zip ngrok
 chmod u+x ngrok
-./ngrok http 8888 --log stdout --log-format logfmt --log-level debug > url.log &
+./ngrok http 80 --log stdout --log-format logfmt --log-level debug > url.log &
