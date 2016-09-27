9cfd1cc6a5d354bc2ffb39043f622538f85f0e3e
Pavel Francírek <pavel@toplist.cz>
2016-09-27 11:40:33 +0200

    Terms link from CA variable
    Not hardcoded terms URL

--- acme_tiny.py
+++ acme_tiny.py
@@ -82,7 +82,7 @@ def get_crt(account_key, csr, acme_dir, log=LOGGER, CA=DEFAULT_CA):
     log.info("Registering account...")
     code, result = _send_signed_request(CA + "/acme/new-reg", {
         "resource": "new-reg",
-        "agreement": requests.head('https://acme-v01.api.letsencrypt.org/terms', allow_redirects=False).headers['location'],
+        "agreement": requests.head(CA + '/terms', allow_redirects=False).headers['location'],
     })
     if code == 201:
         log.info("Registered!")
